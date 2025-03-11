ad99f63d168fc6f4dd7bdf007a622951be85acab
marcelo3design <marcelo3design@gmail.com>
2025-03-11 18:05:46 -0300

    Public1

--- public1/p1.py
+++ public1/p1.py
@@ -1,1 +1,12 @@
-print('Hello World')
+from random import randint
+
+
+
+print(f'Os valores sorteados foram:', end = ' ')
+
+sort = (randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10))
+
+for kdns in range(0,5):
+    print(f'{kdns}', end = ' ~ ')
+print(f'\nO maior valor foi: {max(sort)}')
+print(f'O menor valor foi {min(sort)}')
