2856ebe007fadb5bfd0e8248648789005e2b04e6
marcelo3design <marcelo3design@gmail.com>
2025-04-13 23:38:54 -0300

    Delete public1 directory

--- public1/p1.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-print(f'Os valores sorteados foram:', end = ' ')
-
-sort = (randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10))
-for kdns in range(0,5):
-    print(f'{kdns}', end = ' ~ ')
-print(f'\nO maior valor foi: {max(sort)}')
-print(f'O menor valor foi {min(sort)}')
